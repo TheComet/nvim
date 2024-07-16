e52eae842e51d3cc63bcf69d1864b6e103731af8
Alexander Murray <alexander.murray@franke.com>
2024-07-16 13:29:08 +0200

    Color scenes and telescope

--- lua/thecomet/plugins/colorscheme.lua
+++ lua/thecomet/plugins/colorscheme.lua
@@ -1,10 +1,16 @@
 return {
-    { "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme("catppuccin-mocha")
-        end
-    }
+    { "catppuccin/nvim", name = "catppuccin" },
+    { "tiagovla/tokyodark.nvim",name = "tokyodark" },
+    { "folke/tokyonight.nvim",name = "tokyonight" },
+    { "rebelot/kanagawa.nvim", name = "kanagawa" },
+    { "kepano/flexoki-neovim", name = "flexoki" },
+    { "jacoborus/tender.vim", name = "tender" },
+    { "bluz71/vim-nightfly-colors", name = "nightfly",
+      config = function()
+          vim.cmd.colorscheme("nightfly")
+          vim.keymap.set("n", "<leader>pc", "<CMD>Telescope colorscheme<CR>")
+      end},
+    { "bluz71/vim-moonfly-colors", name = "moonfly" },
+    { "savq/melange-nvim", name = "melange" },
+    { "scottmckendry/cyberdream.nvim", name = "cyberdream" }
 }
-
--- lua/thecomet/plugins/telescope.lua
+++ lua/thecomet/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
+    tag = "0.1.8",
     dependencies = {
         "nvim-lua/plenary.nvim"
     },
@@ -8,15 +8,19 @@ return {
         file_ignore_patterns = {
             "build/",
             "build_cppcheck/",
-        }
+        },
+        pickers = {
+            colorscheme = { enable_preview = true }
+        },
     },
-    config = function()
+    config = function(_, opts)
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', "<leader>pf", builtin.find_files, {})
         vim.keymap.set('n', "<leader>pg", builtin.git_files, {})
         vim.keymap.set('n', "<leader>ps", builtin.live_grep, {})
         vim.keymap.set('n', "gs", builtin.lsp_document_symbols, {})
         vim.keymap.set('n', "gc", builtin.lsp_incoming_calls, {})
+        require("telescope").setup(opts)
     end
 }
 
