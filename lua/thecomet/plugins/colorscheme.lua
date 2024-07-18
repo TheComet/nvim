75814dee528c6b68159a108bbc5c91d38119e723
Alexander Murray <alexander.murray@franke.com>
2024-07-18 08:51:31 +0200

    Adding more color schemes

--- lua/thecomet/plugins/colorscheme.lua
+++ lua/thecomet/plugins/colorscheme.lua
@@ -12,5 +12,25 @@ return {
       end},
     { "bluz71/vim-moonfly-colors", name = "moonfly" },
     { "savq/melange-nvim", name = "melange" },
-    { "scottmckendry/cyberdream.nvim", name = "cyberdream" }
+    { "scottmckendry/cyberdream.nvim", name = "cyberdream" },
+    { "EdenEast/nightfox.nvim", name = "nightfox" },
+    { "sharpchen/Eva-Theme.nvim", name = "Eva-Theme" },
+    { "rose-pine/neovim", name = "rose-pine" },
+    { "DanielEliasib/sweet-fusion", name = "sweet-fusion" },
+    { "neanias/everforest-nvim", name = "everforest" },
+    { "ribru17/bamboo.nvim", name = "bamboo" },
+    { "navarasu/onedark.nvim", name = "onedark" },
+    { "Shatur/neovim-ayu", name = "ayu" },
+    { "oxfist/night-owl.nvim", name = "night-owl" },
+    { "barrientosvctor/abyss.nvim", name = "abyss" },
+    { "mistweaverco/bluloco.nvim", name = "bluloco" },
+    { "dasupradyumna/midnight.nvim", name = "midnight" },
+    { "lettertwo/laserwave.nvim", name = "laserwave" },
+    { "maxmx03/fluoromachine.nvim", name = "fluoromachine", opts = {
+        glow = true, theme = "fluoromachine", transparent = true
+    }},
+    { "lunarvim/synthwave84.nvim", name = "synthwave84" },
+    { "pauchiner/pastelnight.nvim", name = "pastelnight" },
+    { "PunGrumpy/dullahan.nvim", name = "dullahan" },
+    { "Biscuit-Theme/nvim", name = "biscuit" }
 }
